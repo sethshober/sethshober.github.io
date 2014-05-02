390625171f7448b30c81abf728997a8e422cb633
Seth Shober <seth.shober@gmail.com>
2014-05-02 14:00:06 -0400

    updated and linke app js

--- projects/notes/app.js
+++ projects/notes/app.js
@@ -1,38 +1,56 @@
-var editor = $("#editor");
-var display = $("#display");
-var title = $("#title");
+$("#save, #reset").hide();
+$("#note").hide().slideDown(800, function(){
+    $("#save, #reset").fadeIn(function(){
+        $("#note").focus();
+    });
+});
+
+$("#pink").on("click", function() {
+    $(".note").css("background","pink");
+});
+
+$("#green").on("click", function() {
+    $(".note").css("background","seagreen");
+});
+
+ $("#gray").on("click", function() {
+    $(".note").css("background","#eee");
+});
+
+$("#blue").on("click", function() {
+    $(".note").css("background","lightblue");
+});
+
+$("#orange").on("click", function() {
+    $(".note").css("background","orange");
+});
+
+var $note = $("#note");
+var $title = $("#title");
 
 $("#save").click(function(){
     //put editor, display, and title content in variables
-    var editorContent = editor.html();
-    var displayContent = display.html();
-    var titleContent = title.text();
+    var noteContent = $note.html();
+    var titleContent = $title.text();
 
     //create new key values and store in localStorage
-    localStorage.editorContent = editorContent;
-    localStorage.displayContent = displayContent;
+    localStorage.noteContent = noteContent;
     localStorage.titleContent = titleContent;
 });
 
-//if editor content exists in localStorage, load it
-if(localStorage.getItem("editorContent")) {
-    editor.html(localStorage.getItem("editorContent"));
-}
-
 //if display content exists in localStorage, load it
-if(localStorage.getItem("displayContent")) {
-    display.html(localStorage.getItem("displayContent"))
+if(localStorage.getItem("noteContent")) {
+    $note.html(localStorage.getItem("noteContent"))
 }
 
 //if title content exists in localStorage, load it
 if(localStorage.getItem("titleContent")) {
-    title.text(localStorage.getItem("titleContent"))
+    $title.text(localStorage.getItem("titleContent"))
 }
 
 //clear key values in localStorage individually and reload page
 $("#reset").click(function(){
-    localStorage.removeItem("editorContent");
-    localStorage.removeItem("displayContent");
+    localStorage.removeItem("noteContent");
     localStorage.removeItem("titleContent");
     location.reload();
 });
