72410c75d009bf63827575c904918594c6caa799
Seth Shober <seth.shober@gmail.com>
2014-05-04 18:49:50 -0400

    more refactoring and color change now saves

--- projects/notes/app.js
+++ projects/notes/app.js
@@ -1,3 +1,5 @@
+// LOAD ANIMATIONS
+
 //hide save and reset buttons
 $("#save, #reset").hide();
 //slide down note animation
@@ -9,26 +11,30 @@ $("#note").hide().slideDown(800, function(){
     });
 });
 
-//CHANGE COLOR
 
+//CHANGE COLOR
 $(".color").click(function() {
     var $divColor = $(this).css("background");
     $(".note").css("background", $divColor);
 });
 
+
 // SAVING TO LOCALSTORAGE
 
 var $note = $("#note");
 var $title = $("#title");
+var $note = $(".note");
 
 $("#save").click(function(){
     //put editor, display, and title content in variables
     var noteContent = $note.html();
     var titleContent = $title.text();
+    var noteColor = $note.css("background");
 
     //create new key values and store in localStorage
     localStorage.noteContent = noteContent;
     localStorage.titleContent = titleContent;
+    localStorage.noteColor = noteColor;
 });
 
 //if display content exists in localStorage, load it
@@ -41,9 +47,15 @@ if(localStorage.getItem("titleContent")) {
     $title.text(localStorage.getItem("titleContent"))
 }
 
+//if color change exists in localStorage, load it
+if(localStorage.getItem("noteColor")) {
+    $note.css("background", localStorage.getItem("noteColor"))
+}
+
 //clear key values in localStorage individually and reload page
 $("#reset").click(function(){
     localStorage.removeItem("noteContent");
     localStorage.removeItem("titleContent");
+    localStorage.removeItem("noteColor");
     location.reload();
 });
