77d6bc5064bbb324c27180dc716976aa3eb21834
Seth Shober <seth.shober@gmail.com>
2014-08-11 23:36:50 -0700

    minor tweaks

--- projects/flickr/js/app.js
+++ projects/flickr/js/app.js
@@ -1,3 +1,6 @@
+//fast tap
+$('a, label, button, input[type="button"]').fasttap();
+
 $(document).ready(function() {
   
  var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
@@ -9,7 +12,9 @@ $(document).ready(function() {
  	var searchString = $search.val();
 	//prevent submit
  	evt.preventDefault();
-
+ 	$search.prop("disabled",true);
+    $submit.attr("disabled", true).text("Searching...");
+    
  	//AJAXs
  	$.getJSON(
  		flickerAPI,
@@ -24,20 +29,28 @@ $(document).ready(function() {
  				//display them
  				$.each(data.items, function(i,photo){
  					//for each photo returned fill div with it
- 					photoHTML += "<div class='col-xs-12 col-sm-6 col-md-4 col-lg-3'>";
- 					photoHTML += "<img src=" + photo.media.m + " class='img-thumbnail photo'></div>";
+ 					photoHTML += "<div class='col-xs-12 col-sm-6 col-md-4 col-lg-3 test'>";
+ 					photoHTML += "<a href=" + photo.link + " class='photo-link'><img src=" + photo.media.m + " class='img-thumbnail photo' alt='flickr image'></a></div>";
  				}); // end each
  					
  				} else {
  					//if none found tell user
- 					photoHTML = "<p>No photos found matching" + searchString + "</p>";
+ 					photoHTML = "<p class='no-match'>No photos found matching " + searchString + "." + "</p>";
  				}
  				// add to DOM
  				$('#photos').html(photoHTML);
-
+ 				$search.prop("disabled", false);
+      			$submit.attr("disabled", false).text("Search");
 
  	}); // end getJSON
 
  }); // end submit
 
+
 }); // end ready
+
+
+
+
+
+
